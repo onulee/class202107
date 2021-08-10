4897cba65c8dea8e1843f52f35146c8a45332332
onulee <onulee@naver.com>
2021-08-10 17:56:46 +0900

    8월 10일 수업1

---
+++ p0810/src/main/java/com/site/p0810/P0810Application.java
@@ -0,0 +1,13 @@
+package com.site.p0810;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P0810Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P0810Application.class, args);
+	}
+
+}
